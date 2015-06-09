173bef6fab3d25f3135a9502f28331d7e6659ba1
Ben Morimoto <bkmorimoto@gmail.com>
2015-06-09 19:15:37 -0400

    remove turbolinks requirement for javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
 //= require foundation.min.js
 //= require_tree ../../../vendor/assets/javascripts/foundation/
